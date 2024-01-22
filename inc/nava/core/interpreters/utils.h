da673f0206b03e27b1aefe5dcbac1793e8ddda2b
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-22 15:56:44 +0800

    Initial CMake cxx17 lib project for Navascript interpreter.

---
+++ inc/nava/core/interpreters/utils.h
@@ -0,0 +1,19 @@
+#ifndef NAVA_CORE_INTERPRETERS_V2_UTILS_H
+#define NAVA_CORE_INTERPRETERS_V2_UTILS_H
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace interpreters {
+
+class Utils {
+ private:
+ public:
+  Utils();
+  ~Utils();
+};
+}  // namespace interpreters
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
+#endif
---
+++ src/nava/core/interpreters/utils.cc
@@ -0,0 +1,16 @@
+#include "nava/core/interpreters/utils.h"
+
+namespace nvm {
+namespace nava {
+namespace core {
+namespace interpreters
+{
+    
+Utils::Utils() {}
+
+Utils::~Utils() {}
+
+} // namespace interpreters
+}  // namespace core
+}  // namespace nava
+}  // namespace nvm
